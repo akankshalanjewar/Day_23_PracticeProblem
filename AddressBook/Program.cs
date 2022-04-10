5eae7745f9d47e433c3f93344fa1697683009e8f
akankshalanjewar <akankshalanjewar02@gmail.com>
2022-04-10 09:21:56 +0530

    [Akanksha]: Added checking Duplicate Name

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -2,11 +2,55 @@
 
 namespace AddressBook
 {
-    internal class Program
-    {
-        static void Main(string[] args)
+       class Contacts
         {
-            Console.WriteLine("Hello World!");
+            public string firstName { get; set; }
+            public string lastName { get; set; }
+            public string address { get; set; }
+            public string city { get; set; }
+            public string state { get; set; }
+            public string email { get; set; }
+            public int zip { get; set; }
+            public long phoneNumber { get; set; }
+
         }
-    }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Welcome to AddressBook problem");
+
+                Records records = new Records();
+
+
+
+                //Infinite Loop
+                while (true)
+                {
+                    Console.WriteLine("\nEnter 1 to Add person in AddressBook");
+                    Console.WriteLine("Enter 2 to Display");
+                    int choice = int.Parse(Console.ReadLine());
+
+                    switch (choice)
+                    {
+                        case 1:
+                            records.AddRecord();
+                            break;
+                        case 2:
+                            records.ToPrint();
+                            break;
+                        default:
+                            Console.WriteLine("Pleasee select valid input");
+                            break;
+
+                    }
+
+                }
+            }
+        }
+    
 }
+
+
+    
+
